46cca1918840edd9800c9149d18cd03cf4fb8081
Magdalena Stachura <magdalena.stachura@asseco.pl>
2018-12-11 14:53:19 +0100

    test gita

---
+++ AUTOMATYZACJA/src/main/java/Kalkulator/KalkulatorApp.java
@@ -0,0 +1,15 @@
+package Kalkulator;
+
+public class KalkulatorApp {
+
+    public int add(int a, int b) {
+       return a + b;
+    }
+    public double add(double a, double b){
+        return a + b;
+    }
+    public double dzielenie(double a, double b){
+        return a/b;
+    }
+
+}
